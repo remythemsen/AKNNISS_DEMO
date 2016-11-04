614d9f2b2b0d6db7b1e74c1886215cc68c4fad88
remythemsen <remeeh@gmail.com>
2016-11-04 03:02:54 +0100

    Updating files to reflect the version in the AKNNISS repo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,13 +9,13 @@ class Application extends Controller {
   // Initial Index Page
   def index = Action {
     val initQs = Query.lshs.hashTables(0).table.iterator.take(5).map(x => x._2)
-    Ok(views.html.index(initQs.next()))
+    Ok(views.html.index((("000000", Vector()), initQs.next())))
   }
 
   // Query Request
   def query(pictureId:String) = Action {
     val res = Query.getResults(pictureId, 50.0, 50)
-    Ok(views.html.index(res.toList))
+    Ok(views.html.index(res._1, res._2.toList))
   }
 
 
