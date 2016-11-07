e73e08b3b229a71a166d561db9aa93d9dda8d445
remeeh <remeeh@gmail.com>
2016-11-07 17:24:06 +0100

    Adding length test

--- test/preProcessing/DimensionalityReducerSpec.scala
+++ test/preProcessing/DimensionalityReducerSpec.scala
@@ -9,10 +9,25 @@ class DimensionalityReducerSpec extends FlatSpec with Matchers {
     val rnd = new Random(System.currentTimeMillis())
     val A:DenseMatrix[Double]= DimensionalityReducer.getRandMatrix(20000000,4096);
 
-    for(i <- 0 until 1000) {
+    var prev:Vector[Double] = Vector.empty
+    for(i <- 0 until 100) {
       val vIn:Vector[Double] = Vector.fill(4096)(rnd.nextDouble)
       val vOut:Vector[Double] = DimensionalityReducer.getNewVector(vIn,A)
-      (vIn.size == vOut.size) should be (true)
+      if(prev != Vector.empty) {
+        prev.size should be (vOut.size)
+      }
+      prev = vOut
     }
+  }
+
+  "Out Vector" should "be of length 220" in {
+    val rnd = new Random(System.currentTimeMillis())
+    val A:DenseMatrix[Double]= DimensionalityReducer.getRandMatrix(20000000,4096);
+
+    val vIn:Vector[Double] = Vector.fill(4096)(rnd.nextDouble)
+    val vOut:Vector[Double] = DimensionalityReducer.getNewVector(vIn,A)
+
+    vOut.size should be (220)
+    vIn.size should be (4096)
   }
 }
