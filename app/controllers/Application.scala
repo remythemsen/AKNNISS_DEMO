7e78233f2a970f3a87c5b0ef513a7b7b96d57475
remeeh <remeeh@gmail.com>
2016-11-07 20:23:18 +0100

    Fixing distance 0 vs 0 bu´g

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,7 +13,7 @@ class Application extends Controller {
 
   // Query Request
   def query(pictureId:String) = Action {
-    val res = Query.getResults(pictureId,1000.0)
+    val res = Query.getResults(pictureId,1000.0f)
     Ok(views.html.query(res._2, res._1, res._2.length))
   }
 
