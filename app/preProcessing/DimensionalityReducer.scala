059071f89671624a11e53a1f086fa6d96151696e
remeeh <remeeh@gmail.com>
2016-11-07 16:40:10 +0100

    Changing out vector to be of length 220

--- app/preProcessing/DimensionalityReducer.scala
+++ app/preProcessing/DimensionalityReducer.scala
@@ -24,7 +24,7 @@ object DimensionalityReducer{
     val base2 = scala.math.log(2) // natural log of 2
     val log2N = scala.math.log(n) / base2
     // m = new reduced dimension
-    val m=((9*epsilon*log2N).toInt) + 1
+    val m=((9*epsilon*log2N).toInt) + 2
 
     val A = DenseMatrix.rand(m, d, breeze.stats.distributions.Gaussian(0, 1))
     val M=normalizeMatrix(A)
