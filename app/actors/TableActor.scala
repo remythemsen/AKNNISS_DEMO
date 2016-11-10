ee4505d2b9d4ba04bd53a9c42e0a12f641a0b7c1
remythemsen <remeeh@gmail.com>
2016-11-10 22:27:05 +0100

    Replacing old quering and building method with AKKA actor style setup

---
+++ app/actors/TableActor.scala
@@ -0,0 +1,55 @@
+package actors
+
+import IO.Parser
+import LSH.hashFunctions.HashFunction
+import LSH.structures.HashTable
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
+
+case class FillTable(parser:Parser)
+case object GetStatus
+case class Query(q:IndexedSeq[Float])
+case class SaveToDisk(dir:String)
+
+case class QueryResult(items:List[(String, IndexedSeq[Float])])
+
+trait Status
+case object Ready extends Status
+case class InProgress(progress:Int) extends Status
+case object NotReady extends Status
+
+class TableActor(hf:() => HashFunction) extends Actor {
+
+  private val table = new HashTable(hf)
+  private var status:Status = NotReady
+
+  def receive: Receive = {
+
+    // Initializes the TableActor
+    case FillTable(parser) => {
+      Future {
+        for (j <- 0 until parser.size) {
+          status = InProgress(((j.toDouble / parser.size)*100).toInt)
+          table += parser.next
+        }
+      } (ExecutionContext.Implicits.global) onSuccess {
+        case _ => status =  Ready
+      }
+    }
+
+    // Returns the current Status
+    case GetStatus => sender ! status
+
+    // Returns a candidate set for query point
+    case Query(q) => {
+      sender ! {
+        if(status != Ready) Left("Table actor is not ready yet")
+        else Right(QueryResult(table.query(q)))
+      }
+    }
+    case SaveToDisk(dir) => "" // TODO Save the actors current structure to disk
+  }
+}
